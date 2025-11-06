f86aa1ea5b04ae6268eadda487c57a34f3bdc85e
AntonellaSimonetti <antonella.simonetti98@gmail.com>
2025-11-05 22:51:07 -0300

    fix style footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 export const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2025 — Página hecha por <span>Antonella Simonetti</span></p>
+      <p>© 2025 — Página hecha por <span>Antonella Simonetti & Clara Farias</span></p>
     </footer>
   );
 };
